f7708d0eac7a870f9a2aa18664dee6afbdc90194
Luis Fabian Muñoz Ortiz <luiisfabian96@gmail.com>
2020-08-04 23:44:21 -0500

    creacion del modelo de productos con https://app.quicktype.io/\?share\=4Ik8Upww0mN33e2CBVmq

---
+++ lib/src/models/producto_model.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+ProductoModel productoModelFromJson(String str) => ProductoModel.fromJson(json.decode(str));
+
+String productoModelToJson(ProductoModel data) => json.encode(data.toJson());
+
+class ProductoModel {
+    ProductoModel({
+        this.id,
+        this.titulo,
+        this.valor,
+        this.disponible,
+        this.fotoUrl,
+    });
+
+    String id;
+    String titulo ="";
+    double valor = 0.0;
+    bool disponible = true;
+    String fotoUrl;
+
+    factory ProductoModel.fromJson(Map<String, dynamic> json) => ProductoModel(
+        id: json["id"],
+        titulo: json["titulo"],
+        valor: json["valor"],
+        disponible: json["disponible"],
+        fotoUrl: json["fotoURL"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "titulo": titulo,
+        "valor": valor,
+        "disponible": disponible,
+        "fotoURL": fotoUrl,
+    };
+}
