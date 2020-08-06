ecb22381346d93c4ff26020dc6d1888b357663b6
Luis Fabian Muñoz Ortiz <luiisfabian96@gmail.com>
2020-08-06 02:15:08 -0500

    comunicacion entre el modelo y el formulario con el onSaved y el value

--- lib/src/pages/producto_page.dart
+++ lib/src/pages/producto_page.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:login_flutter_bloc/src/models/producto_model.dart';
 import 'package:login_flutter_bloc/utils/utils.dart' as utils;
 
 class ProductoPage extends StatefulWidget {
-
   @override
   _ProductoPageState createState() => _ProductoPageState();
 }
 
 class _ProductoPageState extends State<ProductoPage> {
   final formKey = GlobalKey<FormState>();
+
+  ProductoModel producto = new ProductoModel();
 
   @override
   Widget build(BuildContext context) {
@@ -19,92 +21,105 @@ class _ProductoPageState extends State<ProductoPage> {
           IconButton(
             color: Colors.black,
             icon: Icon(Icons.photo_size_select_actual),
-            onPressed: (){},
+            onPressed: () {},
           ),
           IconButton(
               color: Colors.black,
               icon: Icon(Icons.add_a_photo),
-              onPressed: (){}
-              )
+              onPressed: () {})
         ],
       ),
       body: SingleChildScrollView(
         child: Container(
           padding: EdgeInsets.all(15.0),
           child: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                _crearNombre(),
-                _crearPrecio(),
-                SizedBox(height: 8.0,),
-                _crearBoton(),
-              ],
-            )
-            ),
+              key: formKey,
+              child: Column(
+                children: [
+                  _crearNombre(),
+                  _crearPrecio(),
+                 _crearDisponible(),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  _crearBoton(),
+                ],
+              )),
         ),
       ),
     );
   }
 
-  Widget _crearNombre(){
+  Widget _crearNombre() {
     return TextFormField(
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
         labelText: "Producto",
-
-
       ),
-      validator: (value){
-        if(value.length< 3 ){
+      onSaved: (value) => {producto.titulo = value},
+      validator: (value) {
+        if (value.length < 3) {
           return "ingrese el nombre del producto";
-        }else{
+        } else {
           return null;
         }
       },
     );
   }
 
-  Widget _crearPrecio(){
+  Widget _crearPrecio() {
     return TextFormField(
+      initialValue: producto.valor.toString(),
       keyboardType: TextInputType.number,
       decoration: InputDecoration(
         labelText: "precio",
-        
       ),
-      validator: (value){
+      onSaved: (value) => {producto.valor = double.parse(value)},
+      validator: (value) {
         if (utils.isNumeric(value)) {
           return null;
-        }else{
+        } else {
           return "Solo números";
         }
       },
     );
   }
 
-  Widget _crearBoton(){
+  _crearDisponible() {
+    return SwitchListTile(
+      //value: true,
+        value: producto.disponible,
+        title: Text("Disponible"),
+        activeColor: Colors.green,
+        onChanged: (value) {
+          setState(() {
+             producto.disponible = value;
+          });
+        });
+  }
+
+  Widget _crearBoton() {
     return RaisedButton.icon(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20.0)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       icon: Icon(Icons.save),
       onPressed: _submit,
       label: Text("Guardar"),
       color: Colors.green,
       textColor: Colors.white,
-      
     );
   }
 
-  void _submit(){
+  void _submit() {
+    if (!formKey.currentState.validate()) return;
 
-    if (!formKey.currentState.validate()) return; 
-
-
+    formKey.currentState.save();
     print("good");
+    print(producto.titulo);
+    print(producto.valor);
+
     if (formKey.currentState.validate()) {
       //cuando el formulario es valido
-      
+
     }
   }
 }
